e93e0949c8a8be53d8a1d21deaea5a3fe08c6ade
Indrajit <indrajitsg@gmail.com>
2021-10-01 12:39:22 +0530

    Updated

--- nptel_optimization/lec02_linear_reg.py
+++ nptel_optimization/lec02_linear_reg.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from pandas._config.config import options
+from sklearn.linear_model import LinearRegression
 from scipy.optimize import minimize
 from scipy.optimize import Bounds
 from scipy.optimize import LinearConstraint
@@ -11,22 +12,28 @@ from scipy.optimize import BFGS
 
 def get_data():
     """Generate data"""
-    df = pd.DataFrame({'x': [0, 2, 4, 5, 8, 9],
-                       'y': [2, 3.2, 3.8, 4.6, 6.2, 6.8]})
+    df = pd.DataFrame({'x': [1, 2, 3, 4, 5, 6, 7],
+                       'y': [0.5, 2.5, 2, 4, 3.5, 6, 5.5]})
     return df
+
 
 def eval_fx(b0, b1, x):
     return b0 + b1 * x
 
-def obj(beta):
+
+def obj(beta, type='ss'):
+    """Return model error (Average of Absolute Error"""
     vfunc = np.vectorize(eval_fx)
     df = get_data()
     y_hat = vfunc(beta[0], beta[1], df['x'])
-    # n = df.shape[0]
-    error_abs = np.abs(y_hat - df['y'])
-    return np.mean(error_abs)
+    if type == 'abs':
+        error = np.abs(y_hat - df['y'])
+        return np.mean(error)
+    else:
+        error = np.sum((y_hat - df['y'])**2)
+        return error
 
-bounds = Bounds([-10.0, -10.0], [10.0, 10.0])
+bounds = Bounds([-20.0, -20.0], [20.0, 20.0])
 
 x0 = np.array([1.5, -1.5])
 
@@ -38,3 +45,13 @@ res = minimize(fun=obj,
 print(res)
 
 print(res['x'])
+
+# Check with sklearn LinearRegression
+lm1 = LinearRegression(fit_intercept=True, normalize=False)
+
+df = get_data()
+df.head()
+
+lm1.fit(X=df[['x']], y=df['y'])
+lm1.intercept_, lm1.coef_
+
